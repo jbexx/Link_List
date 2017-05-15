08ef3018bbee733fc0b5ed286933f9df2222d002
David Ryan <djrdev@icloud.com>
2017-05-15 11:03:14 -0600

    JS Skeleton
    JS beginnings

---
+++ scripts.js
@@ -0,0 +1,33 @@
+var titleInput = document.getElementByID('website-title').value;
+var urlInput = document.getElementByID('website-url').value;
+var enterInput = document.getElementByID('enter-button');
+var readInput = document.getElementByID('read');
+var deleteInput = document.getElementByID('delete');
+
+titleInput.addEventListener('click', function () {
+
+});
+
+urlInput.addEventListener('click', function () {
+
+});
+
+enterInput.addEventListener('click', function () {
+
+});
+
+readInput.addEventListener('click', function () {
+
+});
+
+deleteInput.addEventListener('click', function () {
+
+});
+
+function recordPrepend() {
+
+}
+
+function createRecord(title, url) {
+
+}
