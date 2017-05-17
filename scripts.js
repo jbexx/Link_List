d5856f67b9622891e021d18e212f5b17c7a15d86
David Ryan <djrdev@icloud.com>
2017-05-16 19:19:41 -0600

    disable enter button and add class when read button is clicked

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 
+// =========Variables=========
+
 var titleInput = $('#website-title').val();
 var urlInput = $('#website-url').val();
 var enterInput = $('#enter-button');
@@ -6,49 +8,39 @@ var readInput = $('#read');
 var deleteInput = $('#delete');
 var recordCount = 0;
 
-$('#website-title').click(function () {
+// ==========Event Listeners========
 
+$('.input').on('keyup', disableBtn);
+
+$('#enter-button').on('click', function() {
+  createRecord()
+  $('#website-title').val('');
+  $('#website-url').val('');
+  disableBtn();
 });
 
-$('#website-url').click(function () {
-
-});
-
-$('#enter-button').on('click', createRecord);
-
-$('.child-2').click(function () {
-  readRecord();
-});
+$('.child-2').on('click', '.read-btn', readRecord);
 
 $('.child-2').on('click', '.delete-btn', deleteRecord);
-
-// $(function () {
-//   $('div.bookmark-card').toggleClass('read');
-// });
-
-// function recordPrepend() {
-//   var titleInput = $('#website-title').val();
-//   var urlInput = $('#website-url').val();
-//   creatRecord(titleInput, urlInput);
-
-
-//}
 
 function createRecord() {
   var titleInput = $('#website-title').val();
   var urlInput = $('#website-url').val();
 
-    if (titleInput === null) {
+    if (titleInput === '') {
       alert("You are missing the title!")
-    } else if (urlInput === null) {
+    } else if (urlInput === '') {
       alert("You are missing the URL!")
-    } else if (!validURL(urlInput)) {
+    } else if (!validUrl(urlInput)) {
       alert("Your URL is invalid!")
     } else {
       recordCount++;
       inject(titleInput, urlInput);
+      console.log(recordCount);
     }
 }
+
+// ===========Functions===========
 
 function inject(title, url) {$('.child-2').prepend(`
   <div class="bookmark">
@@ -56,41 +48,53 @@ function inject(title, url) {$('.child-2').prepend(`
     <hr>
     <a href="${url}" class="siteurl clickable">${url}</a>
     <hr>
-      <div class="flex">
-      <button class="read-btn clickable" name="read-btn" type="button">Read</button>
-      <button class="delete-btn clickable" name="delete-btn" type="button">Delete</button>
+      <div class="read-delete-buttons">
+        <button class="read-btn clickable" name="read-btn" type="button">Read</button>
+        <button class="delete-btn clickable" name="delete-btn" type="button">Delete</button>
       </div>
   </div>
 `)
 }
 
 function readRecord() {
-  $( this ).parent().toggleClass('read');
+  $(this).parent().parent().toggleClass('read-card');
+  $(this).toggleClass('red-btn');
+  console.log('it runs');
 }
 
 function deleteRecord() {
-    recordCount--;
-    $( this ).parent().remove;
+  recordCount--;
+  $(this).parent().parent().remove();
+  console.log(recordCount);
 }
 
-function readRecordCount(x) {
-  return $('.read').length;
+function readRecordCount() {
+  return $('.read-card').length;
 }
 
-function unreadRecordCount(x) {
+// function validUrl(url) {
+//   var pattern = new RegExp('^(https?:\\/\\/)?'+
+//     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+
+//     '((\\d{1,3}\\.){3}\\d{1,3}))'+
+//     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+//     '(\\?[;&a-z\\d%_.~+=-]*)?'+
+//     '(\\#[-a-z\\d_]*)?$','i');
+//     console.log(pattern.test(url));
+//  return pattern.test(url);
+//  }
+
+function disableBtn() {
+  if ($('#website-title').val() !== '' && $('#website-url').val() !== '') {
+    $('#enter-button').prop('disabled', false);
+  } else {
+      $('#enter-button').prop('disabled', true);
+  }
+}
+
+function unreadRecordCount() {
   return recordCount - readRecordCount();
 }
 
 function clearRead() {
-  $( ".read" ).remove();
+  $( ".read-card" ).remove();
 }
-
-function validURL(url) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+
-    '(\\#[-a-z\\d_]*)?$','i');
- return pattern.test(url);
- }
